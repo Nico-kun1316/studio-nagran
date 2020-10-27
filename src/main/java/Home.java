77df51395d589ea1983e9e0db26c0c574f53f691
Nico-kun <gobisek1@gmail.com>
2020-10-27 19:17:34 +0100

    update sample code

--- src/main/java/Home.java
+++ src/main/java/Home.java
@@ -1,2 +1,20 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
 public class Home {
+    public TextField source;
+    public TextField target;
+    public Label label;
+
+    @FXML
+    public void initialize() {
+        target.textProperty().bind(source.textProperty());
+    }
+
+    public void makeBlue(ActionEvent event) {
+        label.getStyleClass().removeIf(className -> className.equals("red"));
+        label.getStyleClass().add("blue");
+    }
 }
